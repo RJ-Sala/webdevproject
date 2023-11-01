7078f1565f7c158e12631efe8817bce8c33f201d
RJ-Sala <qb202100198@wmsu.edu.ph>
2023-11-01 17:49:36 +0800

    added some code

---
+++ functions/delete.php
@@ -0,0 +1,10 @@
+<?php
+    //including the databaseconnection file
+    include_once("../dbConnection/mysqlconfig_connection.php");
+    //getting id of the data from url
+    $id = $_GET['id'];
+    //deleting the row from the table
+    mysqli_query($dbc, "DELETE FROM tblsubject WHERE subject_id='$id'");
+    //redirecting to the display page (index.php in our case)
+    header("Location:../index.php")
+?>
