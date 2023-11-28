93d15b262176680ef41b41a7b8186c24fb98326e
RJ-Sala <qb202100198@wmsu.edu.ph>
2023-11-28 22:24:14 +0800

    updated

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,6 +1,6 @@
 <?php
     //fetching data
-    $query = "SELECT all entries from tblsubjects
+    $query = "SELECT * from tblsubjects
                 LEFT JOIN tblsyllabus
                 ON tblsyllabus.subject_id = tblsubjects_id";
     $result = mysqli_query($dbc, $query); // using mysqli_query
